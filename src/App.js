83d5e321a31a8e35d80e356e32478162818c1852
Izabella Ribeiro <bellarpjf@gmail.com>
2020-07-29 01:29:11 -0300

    Adicionando conteudo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Vocês já ouviram uma das mais incriveis bandas nacionais?? Se não conhecem ainda esse é o momento perfeito"}
       />
 
       <Carousel
